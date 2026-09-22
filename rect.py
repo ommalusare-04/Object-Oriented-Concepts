e062b89ba51e36113903fa6b8e25b1fce6cce5be
Om Malusare <omalusare850@gmail.com>
2026-09-22 10:37:47 +0630

    commit

--- rect.py
+++ rect.py
@@ -1,1 +1,14 @@
-class 
+class rect:
+    def __init__(self, height, width):
+        self.height = height
+        self.width = width
+
+    def area(self):
+        return self.height * self.width
+
+    def displayArea(self):
+        print(f"Area of rectangle is {self.area()}")
+
+
+r1 = rect(10, 3)
+r1.displayArea()
