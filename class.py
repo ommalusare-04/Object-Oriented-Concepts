59f57a8624c94b1f105fcd16b3d63c16d48cc4ed
Om Malusare <omalusare850@gmail.com>
2026-09-22 10:29:05 +0630

    commit

--- class.py
+++ class.py
@@ -6,10 +6,19 @@ class Vector2D:
     def add(self, v):
         return Vector2D(self.x + v.x, self.y + v.y)
 
+    def sub(self, v):
+        return Vector2D(self.x - v.x, self.y - v.y)
+
     def display(self):
         print("(", self.x, ",", self.y, ")")
 
 v1 = Vector2D(2, 3)
 v2 = Vector2D(4, 5)
 v3 = v1.add(v2)
+
+v3.display()
+
+v1=Vector2D(4,6)
+v2=Vector2D(2,3)
+v3=v1.sub(v2)
 v3.display()
